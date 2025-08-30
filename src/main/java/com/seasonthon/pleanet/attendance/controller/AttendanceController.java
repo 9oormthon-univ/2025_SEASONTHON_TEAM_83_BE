9feafd9dc22502ba4d958c72c5a74b4e847f768b
lgr0831 <qfrub0@gmail.com>
2025-08-30 23:29:06 +0900

    fix: [Attendance] 월별 출석 현황 조회 API 수정

--- src/main/java/com/seasonthon/pleanet/attendance/controller/AttendanceController.java
+++ src/main/java/com/seasonthon/pleanet/attendance/controller/AttendanceController.java
@@ -3,8 +3,10 @@ package com.seasonthon.pleanet.attendance.controller;
 import com.seasonthon.pleanet.attendance.domain.Attendance;
 import com.seasonthon.pleanet.attendance.dto.AttendanceResponseDto;
 import com.seasonthon.pleanet.attendance.service.AttendanceService;
+import com.seasonthon.pleanet.common.config.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +21,13 @@ public class AttendanceController {
 
     @GetMapping("/monthly")
     public ResponseEntity<List<AttendanceResponseDto>> getMonthlyAttendance(
-            @RequestParam Long memberId,
             @RequestParam int year,
-            @RequestParam int month) {
+            @RequestParam int month,
+            Authentication authentication) { // 로그인 정보 주입받음
+
+        // 토큰/세션에서 로그인된 사용자 꺼내오기
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long memberId = userDetails.getId();
 
         List<Attendance> attendances = attendanceService.getMonthlyAttendance(memberId, year, month);
 
