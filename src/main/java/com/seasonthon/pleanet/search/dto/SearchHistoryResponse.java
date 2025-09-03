43de3aaeb70a6a54996028901f87531d434f0055
lgr0831 <qfrub0@gmail.com>
2025-09-03 20:22:52 +0900

    feat: [Search] 최근 검색어 조회 API 구현

---
+++ src/main/java/com/seasonthon/pleanet/search/dto/SearchHistoryResponse.java
@@ -0,0 +1,11 @@
+package com.seasonthon.pleanet.search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SearchHistoryResponse {
+    private String keyword;
+}
+
--- src/main/java/com/seasonthon/pleanet/search/repository/SearchHistoryRepository.java
+++ src/main/java/com/seasonthon/pleanet/search/repository/SearchHistoryRepository.java
@@ -9,5 +9,8 @@ import java.util.List;
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
     // 유저별 최근 검색어
     List<SearchHistory> findByMemberOrderByCreatedAtDesc(Member member);
+
+    // 유저의 최근 검색어 5개 (최신순)
+    List<SearchHistory> findTop5ByMemberOrderByCreatedAtDesc(Member member);
 }
 
