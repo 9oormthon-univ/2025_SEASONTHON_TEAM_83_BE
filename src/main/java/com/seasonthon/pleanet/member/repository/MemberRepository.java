08f2cc5f9b332db1c02301158f15018ef2d41c1c
lgr0831 <qfrub0@gmail.com>
2025-09-03 19:36:12 +0900

    feat: [search] 통합 검색 API 구현

--- src/main/java/com/seasonthon/pleanet/member/repository/MemberRepository.java
+++ src/main/java/com/seasonthon/pleanet/member/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import com.seasonthon.pleanet.member.domain.enums.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,8 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
     Optional<Member> findByNickname(String nickname);
+    List<Member> findByNicknameContaining(String keyword);  // 부분 일치 검색
     Optional<Member> findBySocialId(Long socialId);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
-
 }
